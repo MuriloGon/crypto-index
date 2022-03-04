f24b2173d9f5755866a9919d8ee4378aef3c0b1f
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 04:32:03 -0300

    test(back): add generateToken test to match token with letters and numbers

--- backend/tests/unit/auth.service.spec.ts
+++ backend/tests/unit/auth.service.spec.ts
@@ -5,7 +5,7 @@ import {generateToken, login} from '../../src/services/login';
 describe('Auth service - generateToken function', () => {
   it('returns a 16 characters and number token by default', async () => {
     const received = generateToken();
-    expect(received).to.have.length(12);
+    expect(received).to.have.length(16);
   });
   it('returns a custom characters token', async () => {
     const received1 = generateToken(5);
@@ -14,6 +14,11 @@ describe('Auth service - generateToken function', () => {
     expect(received1).to.have.length(5);
     expect(received2).to.have.length(10);
     expect(received3).to.have.length(15);
+  });
+  it('It returns a random 16 letters and numbers token', () => {
+    const received = generateToken();
+    expect(received).to.has.length(16);
+    expect(received).to.match(/[A-Za-z0-9]*([a-zA-Z]+[0-9]+|[0-9]+[a-zA-Z]+)/);
   });
 });
 
@@ -36,15 +41,15 @@ describe('Auth service - login function', () => {
     expect(received3).to.be.eq(null);
   });
 
-  it('returns a 12 characters token when the input is valid', async () => {
+  it('returns a 16 characters token when the input is valid', async () => {
     const mockData1 = {email: 'email@email.com.br', password: '123456'};
     const mockData2 = {email: 'emai2l@google.com', password: 'abcdef'};
     const mockData3 = {email: 'emailcc@apple.uk', password: '1a2b3c'};
     const received1 = await login(mockData1);
     const received2 = await login(mockData2);
     const received3 = await login(mockData3);
-    expect(received1).to.have.length(12);
-    expect(received2).to.have.length(12);
-    expect(received3).to.have.length(12);
+    expect(received1).to.have.length(16);
+    expect(received2).to.have.length(16);
+    expect(received3).to.have.length(16);
   });
 });
