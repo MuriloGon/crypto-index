d1cb684c96c9213c2ab55ab7066276d33115287f
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 09:42:04 -0300

    feat(back): implement authentication middleware

--- backend/src/routes/crypto.ts
+++ backend/src/routes/crypto.ts
@@ -1,8 +1,11 @@
 import {Router} from 'express';
 import * as cryptoControllers from '../controllers/crypto';
 import * as cryptoMiddlewares from '../middlewares/crypto';
+import * as authMiddlewares from '../middlewares/auth';
 
 const router = Router();
+
+router.use(authMiddlewares.authentication);
 
 router.get('/btc', cryptoControllers.getBtcCurrencies);
 
