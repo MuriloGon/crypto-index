b8867832c96d9181cf4e6671e51fbd38cdcf0b12
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 08:46:59 -0300

    feat(back): implement updateLocalCurrency crypto controller

--- backend/src/controllers/crypto.ts
+++ backend/src/controllers/crypto.ts
@@ -12,3 +12,18 @@ export async function getBtcCurrencies(req: Request, res: Response) {
   }
   res.status(200).json(customCurrenciesRate);
 }
+
+export async function updateLocalCurrency(req: Request, res: Response) {
+  const {currency: code, value} = req.body;
+
+  const updateResponse = cryptoService.updateLocalCurrency(
+      code,
+      value,
+      process.env.CURRENCIES_PATH || 'currencies.json',
+  );
+
+  if (updateResponse === null) {
+    return res.status(500).json(ErrorResponse('Internal error'));
+  }
+  res.status(200).json({message: 'Valor alterado com sucesso!'});
+}
