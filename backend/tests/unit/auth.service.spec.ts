6824b6e6e1462a53a8b73a62c723c00fffb6a77e
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 04:13:06 -0300

    test(back): add generateToken tests

--- backend/tests/unit/auth.service.spec.ts
+++ backend/tests/unit/auth.service.spec.ts
@@ -1,6 +1,21 @@
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
-import {login} from '../../src/services/login';
+import {generateToken, login} from '../../src/services/login';
+
+describe('Auth service - generateToken function', () => {
+  it('returns a 16 characters and number token by default', async () => {
+    const received = generateToken();
+    expect(received).to.have.length(12);
+  });
+  it('returns a custom characters token', async () => {
+    const received1 = generateToken(5);
+    const received2 = generateToken(10);
+    const received3 = generateToken(15);
+    expect(received1).to.have.length(5);
+    expect(received2).to.have.length(10);
+    expect(received3).to.have.length(15);
+  });
+});
 
 describe('Auth service - login function', () => {
   it('returns null when email is invalid', async () => {
@@ -33,4 +48,3 @@ describe('Auth service - login function', () => {
     expect(received3).to.have.length(12);
   });
 });
-
