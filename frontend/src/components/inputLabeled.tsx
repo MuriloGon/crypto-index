0cea7dcb42c3a076c4733a320c8972b1840e7c01
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 17:31:03 -0300

    feat(front): implement inputLabeled component

--- frontend/src/components/inputLabeled.tsx
+++ frontend/src/components/inputLabeled.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function InputLabeled() {
+import './inputLabeled.css'
+
+type props = {
+  type: string
+  id: string
+  value: string
+  label: string
+  onChange: (newValue: string) => void
+}
+
+function InputLabeled({ type, id, label, value, onChange }: props) {
   return (
-    <div>
-      InputLabeled
+    <div className="input-label">
+      <label htmlFor={id}>
+        {label}
+      </label>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        onChange={({ target: { value } }) => onChange(value)}
+      />
     </div>
-  )
+  );
 }
 
 export default InputLabeled
