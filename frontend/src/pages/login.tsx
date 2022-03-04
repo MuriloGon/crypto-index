b57507662909fddf75705e6d5ee2f428b8a46c6c
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 18:07:40 -0300

    feat(front): implement login page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/button";
+import InputLabeled from "../components/inputLabeled";
+import './login.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [pwd, setPwd] = useState('');
+
   return (
-    <div>
-      Login
+    <div className="login-container">
+      <InputLabeled
+        id="login-email"
+        label="Email"
+        type="email"
+        onChange={setEmail}
+        value={email}
+      />
+      <InputLabeled
+        id="login-pwd"
+        label="Password"
+        type="password"
+        onChange={setPwd}
+        value={pwd}
+      />
+      <Button color="primary" text="Entrar" onClick={() => alert('clicado')} />
     </div>
   )
 }
