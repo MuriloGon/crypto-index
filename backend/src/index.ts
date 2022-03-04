fc2519e34c91aad743f37cd7ceea23aa560911d0
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-03 23:22:51 -0300

    build(back): add initial boilerplate

---
+++ backend/src/index.ts
@@ -0,0 +1,3 @@
+console.log('test');
+
+export const x = () => 10;
---
+++ backend/tests/teste.spec.ts
@@ -0,0 +1,10 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+
+
+describe('teste', () => {
+  it('test', () => {
+    expect(1+1).to.be.eq(3);
+  });
+});
+
