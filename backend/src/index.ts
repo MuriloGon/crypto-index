1b2e5c5e090db16e77dde5339e532e442137293a
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 12:04:32 -0300

    build(back): add dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import api from './api';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 api.listen(PORT, () => {
   console.log(`Listening port ${PORT}`);
