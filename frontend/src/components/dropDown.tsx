8be1e933cad27c74e5923a4912793ed1a2ebb547
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 17:38:04 -0300

    feat(front): implement dropDown behaviour

--- frontend/src/components/dropDown.tsx
+++ frontend/src/components/dropDown.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import './dropDown.css'
 
-function DropDown() {
+type props = {
+  options: Array<string>
+  value: string
+  label: string
+  onChange: (newValue: string) => void
+}
+
+function DropDown({ options, label, value, onChange }: props) {
   return (
-    <div>
-      DropDown
+    <div className="custom-select">
+      <label>{label}</label>
+      <select value={value} onChange={(x) => onChange(x.target.value)}>
+        {options.map((op, index) => (
+          <option key={op + index} value={op}>{op}</option>
+        ))}
+      </select>
     </div>
   )
 }
