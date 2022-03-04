bef8d364ca256bd43fb0d21c8204a1b5ae038960
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 06:30:01 -0300

    test(back): add getLocalCurrent function tests

--- backend/tests/unit/crypto.service.spec.ts
+++ backend/tests/unit/crypto.service.spec.ts
@@ -1,6 +1,7 @@
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
-import {setCurrency} from '../../src/services/crypto';
+import {getLocalCurrencies, setCurrency} from '../../src/services/crypto';
+
 
 describe('Unit - Crypto - Service - setCurrency function', () => {
   it('returns null when currencyCode is not BRL, EUR e CAD or the value ' +
@@ -18,4 +19,19 @@ describe('Unit - Crypto - Service - setCurrency function', () => {
         'CAD', 1000, {BRL: '1', CAD: '2', EUR: '3'});
     expect(received1).to.be.eql({BRL: '1', CAD: '1000', EUR: '3'});
   });
+});
+
+describe('Unit - Crypto - Service - getLocalCurrencies function', () => {
+  it('returns the currencies.json for a valid path',
+      async () => {
+        const received = await getLocalCurrencies('data/currencies.json');
+        expect(received).to.have.keys('BRL', 'EUR', 'CAD');
+      },
+  );
+  it('returns null for a invalid path',
+      async () => {
+        const received = await getLocalCurrencies('data/currencies.inexistent');
+        expect(received).to.be.null;
+      },
+  );
 });
