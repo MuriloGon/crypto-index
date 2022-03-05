a25f1fedf3b5cda1b66f49f1cf241176fd1a83b0
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 21:41:38 -0300

    feat(front): implement home page

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import Button from "../components/button";
+import PriceLabeled from "../components/priceLabeled";
+
+import "./home.css"
 
 function Home() {
+  const currencies = ['A', 'B', 'C', 'D'];
+
   return (
-    <div>
-      <Button text="Teste" color="primary" onClick={() => {}} />
+    <div className="home-container">
+      <Button
+        text="Atualizar valor monetário"
+        color="secondary"
+        onClick={() => { }}
+      />
+
+      <div className="currencies-prices">
+        <div className="currencies-btc">
+          <PriceLabeled code="BRC" price="x,xxx" />
+        </div>
+        <div className="currencies-others">
+          {currencies.map((x, i) => (
+            <PriceLabeled code="BRC" price="123,123123" />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
