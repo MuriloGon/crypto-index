1169a19cc17fe739b7e8abbf8025483969c84c41
Murilo Gonçalves <murilogoncalves@gmail.com>
2022-03-04 23:21:02 -0300

    feat(front): implement login behaviours

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api";
 import Button from "../components/button";
 import InputLabeled from "../components/inputLabeled";
+import { setToken } from "../utils/token";
 import './login.css';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [pwd, setPwd] = useState('');
+  const nav = useNavigate();
+
+  function loginUser() {
+    api.login.login({ email, password: pwd })
+      .then(x => setToken(x.token))
+      .then(() => { nav('/'); })
+      .catch(x => alert(x.message));
+  }
 
   return (
     <div className="login-container">
@@ -23,7 +34,7 @@ function Login() {
         onChange={setPwd}
         value={pwd}
       />
-      <Button color="primary" text="Entrar" onClick={() => alert('clicado')} />
+      <Button color="primary" text="Entrar" onClick={loginUser} />
     </div>
   )
 }
